bae8948b7d47a7258f76fd63b46443b2c41bfd58
josue2h <huarachi.herrera.josue@gmail.com>
2021-07-07 18:06:32 -0400

    2 cambio

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-hola
+require ('dotenv').config()
+const http = require('http')
+
+//const hostName = '127.0.0.1'
+
+//const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('hello word')
+})
+
+server.listen(process.env.DB_PORT, process.env.DB_HOST, () => {
+  console.log(`listen server http://${process.env.DB_HOST}:${process.env.DB_PORT}`)
+})
